4bc347ba8168a3553f324c8d7c9399a8607c7dda
wonseokchoi <wonseokchoi@nts-corp.com>
2025-08-11 17:57:07 +0900

    update

--- src/constants/projectTweets.ts
+++ src/constants/projectTweets.ts
@@ -29,6 +29,13 @@ export const PROJECT_TWEETS: ProjectTweet[] = [
     tweetUrl: 'https://x.com/Jst_Mariee/status/1953494872888955018',
     tweetId: '1953494872888955018'
   },
+  {
+    projectId: 'irysdune',
+    projectName: 'IrysDune',
+    projectIcon: APP_PRESETS.find(p => p.id === 'irysdune')?.icon || null,
+    tweetUrl: 'https://x.com/0xCrocy/status/1951799943187829167',
+    tweetId: '1951799943187829167'
+  },
   // BridgBox
   {
     projectId: 'bridgebox',
@@ -36,6 +43,37 @@ export const PROJECT_TWEETS: ProjectTweet[] = [
     projectIcon: APP_PRESETS.find(p => p.id === 'bridgebox')?.icon || null,
     tweetUrl: 'https://x.com/Jst_Mariee/status/1952385559646601677',
     tweetId: '1952385559646601677'
+  },
+  {
+    projectId: 'bridgebox',
+    projectName: 'BridgBox',
+    projectIcon: APP_PRESETS.find(p => p.id === 'bridgebox')?.icon || null,
+    tweetUrl: 'https://x.com/0xCrocy/status/1952543921013182567',
+    tweetId: '1952543921013182567'
+  },
+  // Irys Flip
+  {
+    projectId: 'irysflip',
+    projectName: 'Irys Flip',
+    projectIcon: APP_PRESETS.find(p => p.id === 'irysflip')?.icon || null,
+    tweetUrl: 'https://x.com/0xCrocy/status/1953917125157847440',
+    tweetId: '1953917125157847440'
+  },
+  // Irys Realms
+  {
+    projectId: 'irys-realms',
+    projectName: 'Irys Realms',
+    projectIcon: APP_PRESETS.find(p => p.id === 'irys-realms')?.icon || null,
+    tweetUrl: 'https://x.com/0xCrocy/status/1951054949481119751',
+    tweetId: '1951054949481119751'
+  },
+  // cm's note
+  {
+    projectId: 'cm-note',
+    projectName: 'CM Note',
+    projectIcon: APP_PRESETS.find(p => p.id === 'cm-note')?.icon || null,
+    tweetUrl: 'https://x.com/0xCrocy/status/1950630852762767630',
+    tweetId: '1950630852762767630'
   }
 ];
 
